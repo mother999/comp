82e77fe35eea4c0c562b409cc7ded671d0897f29
Fazli Razak <thosai96@gmail.com>
2023-05-25 20:29:12 +0800

    initial commit

---
+++ data/clothing/clHats.js
@@ -0,0 +1,30 @@
+SugarCube.setup.clHats = {
+    "none":{
+        "name":"None",
+        "id":"none",
+        "dir":"none",
+        "img":"none",
+        "tags":[],
+        "main":"none",
+        "top":"",
+        "bottom":"",
+        "socks":"",
+        "shoes":"",
+        "store":"none",
+        "cost": 0
+    },
+    "newsboy-hat":{
+        "name":"Newsboy Hat",
+        "id":"newsboy-hat",
+        "dir":"newsboy-hat",
+        "img":["base","base","base","base","base","base"],
+        "tags":[""],
+        "main":"",
+        "top":"",
+        "bottom":"",
+        "socks":"",
+        "shoes":"",
+        "store":"none",
+        "cost":0
+    }
+};
